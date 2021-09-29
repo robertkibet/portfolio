1e7ec3cc61b214a572a3fb97f67603102c17d9f1
Robert Kibet <kipronokrobert@gmail.com>
2021-09-29 23:33:37 +0300

    cleanup

--- src/services/logger/index.js
+++ src/services/logger/index.js
@@ -2,10 +2,10 @@
 import * as Sentry from '@sentry/nextjs';
 
 const sentryLogger = (message) => {
-  // if (process.env.NEXT_PUBLIC_ENVIRONMENT === 'development') {
-  //   console.log('error occured', message);
-  //   return;
-  // }
+  if (process.env.NEXT_PUBLIC_ENVIRONMENT === 'development') {
+    console.log('error occured', message);
+    return;
+  }
 
   Sentry.withScope((scope) => {
     scope.setExtra('section', message?.section || undefined);
