36581b96ae2fe51083cd0642e7b27f7e9e88e9af
Robert Kibet <kipronokrobert@gmail.com>
2021-09-30 00:02:58 +0300

    cleanup 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,7 @@
 // eslint-disable
 /* eslint-disable */
-import { useRouter } from 'next/router';
 import React from 'react';
-// import sentryLogger from "../services/logger";
 
 export default function Custom404() {
-  const router = useRouter();
-  // React.useEffect(() => {
-  //   sentryLogger({
-  //     component: '404',
-  //     section: '404',
-  //     error: `page not found ${router.pathname}` 
-  //   })    
-  // },[])
-  
   return <h1>404 - Page Not Found</h1>
 };
