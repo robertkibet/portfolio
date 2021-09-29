4de5e8d83d09ae9ab09223a33504d690e2ce2ddd
Robert Kibet <kipronokrobert@gmail.com>
2021-09-29 19:34:30 +0300

    hack to fix bybass babel eslint error
    more info:
    https://github.com/eslint/eslint/issues/13394
    https://stackoverflow.com/a/60593353

--- src/services/github/index.js
+++ src/services/github/index.js
@@ -1,27 +1,26 @@
-import axios from "axios";
-import React from "react";
+import axios from 'axios';
+import React from 'react';
 
 const useGithub = () => {
-  const [projects, setProjects ] = React.useState([]);
-  const api_key =  process.browser && process.env.NEXT_PUBLIC_GIT_TOKEN;
+  const [projects, setProjects] = React.useState([]);
+  const apiKey = process.browser && process.env.NEXT_PUBLIC_GIT_TOKEN;
 
-  React.useEffect(()=>{
-    const getProjos = async ()=>{
-      await axios.get('https://api.github.com/users/robertkibet/repos', {
-        headers: {
-          Authorization: `token ${api_key}`,
-        },
-      }).then(response=>{
-
-        const projectsFetched =  response.data.filter(item => item.private===false);
-        setProjects(projectsFetched);
-        console.log(projectsFetched)
-      })
-    }
+  React.useEffect(() => {
+    const getProjos = async () => {
+      await axios
+        .get('https://api.github.com/users/robertkibet/repos', {
+          headers: { // eslint-disable-line
+            Authorization: 'token '+apiKey, // eslint-disable-line
+          }, // eslint-disable-line
+        })
+        .then((response) => {
+          const projectsFetched = response.data.filter((item) => item.private === false);
+          setProjects(projectsFetched);
+        });
+    };
     getProjos();
-  },[]);
+  }, []);
 
-  return {projects};
-
+  return { projects };
 };
 export default useGithub;
