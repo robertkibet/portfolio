eaa17c9ccc7977e2fbbc650f99cd455ad22f3b75
Robert Kibet <kipronokrobert@gmail.com>
2022-04-15 17:30:48 +0300

    new:  add blog functionality and save to firestore

--- src/screens/dashboard/add-blog/index.js
+++ src/screens/dashboard/add-blog/index.js
@@ -4,6 +4,7 @@ import * as Yup from 'yup';
 import { Formik } from 'formik';
 import { EditorState, convertToRaw } from 'draft-js';
 import draftToHtml from 'draftjs-to-html';
+import { doc, setDoc } from 'firebase/firestore';
 
 import Text from '../../../components/text';
 import Editor from '../../../components/editor';
@@ -20,44 +21,49 @@ import {
   HelperMessage,
 } from './styles';
 
-import DismissableAlert from '../../../components/alerts/dismissable-alert';
 import useAuth from '../../../containers/useAuth';
+import { firestore } from '../../../services/firebase';
 
 const blogSchema = Yup.object().shape({
-  email: Yup.string()
-    .required('Enter your email')
-    .test('is-accepted', 'This email is not accepted', (value) => {
-      if (value) {
-        return value.trim().endsWith('@kibet.me');
-      }
-      return false;
-    }),
-  password: Yup.string().required('password is required'),
+  title: Yup.string().required('Enter title'),
+  categories: Yup.array().min(1, 'Select at least one category'),
 });
 
-const AddBlog = ({ submitBlog }) => {
+const AddBlog = () => {
   const [editorState, setEditorState] = React.useState(EditorState.createEmpty());
+  const [isSubmitting, setIsSubmitting] = React.useState(false);
 
   const router = useRouter();
   const auth = useAuth();
-  const { state, login, isLoginIn } = auth;
+  const { state } = auth;
 
-  const handleAddBlog = (values) => {
+  const handleAddBlog = async (values) => {
     const content = draftToHtml(convertToRaw(editorState.getCurrentContent()));
-
+    setIsSubmitting(true);
     try {
-      console.log('submitBlog', {
+      const url = encodeURIComponent(values.title.toLowerCase().replace(/[^a-z0-9 _-]+/gi, '-'));
+
+      const extendedBlogUrl = `${new Date().toISOString().slice(0, 10)}-${url}`;
+
+      await setDoc(doc(firestore, 'ARTICLES', extendedBlogUrl), {
+        url,
+        categories: values.categories,
+        title: values.title,
         content,
-        values,
+        dateAdded: new Date().toISOString().slice(0, 10),
+        addedBy: state.user.email,
       });
+
+      router.push(`/dashboard/blog/${url}`);
     } catch (error) {
-      console.log(error);
+      alert(error?.message || 'Failed to save this article');
+    } finally {
+      setIsSubmitting(false);
     }
   };
 
   return (
     <Wrapper>
-      {!isLoginIn && state?.error && <DismissableAlert severity="error" message={state?.error} />}
       <AddBlogWrapper>
         <HeaderSection>
           <Text
@@ -93,26 +99,36 @@ const AddBlog = ({ submitBlog }) => {
                     label="Title"
                     variant="outlined"
                     size="small"
-                    name="title"
                   />
                   {!!errors.title && <HelperMessage error={!!errors.title}>{errors.title}</HelperMessage>}
                 </FieldWrapper>
+                <FieldWrapper required error={!!errors.categories} variant="standard">
+                  <select
+                    multiple
+                    required
+                    name="categories"
+                    id="categories"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.categories}
+                  >
+                    <option value="technology">Technology</option>
+                    <option value="art">Art</option>
+                    <option value="programming">Programming</option>
+                    <option value="javascript">JavaScript</option>
+                    <option value="rust">Rust</option>
+                    <option value="typescrit">TypeScript</option>
+                    <option value="mongo-db">Mongo DB</option>
+                    <option value="nodejs">NodeJs</option>
+                    <option value="react">React</option>
+                    <option value="nextjs">Nextjs</option>
+                  </select>
+                  {!!errors.categories && <HelperMessage error={!!errors.categories}>{errors.categories}</HelperMessage>}
+                </FieldWrapper>
 
-                <select multiple required name="categories" id="categories">
-                  <option value="technology">Technology</option>
-                  <option value="art">Art</option>
-                  <option value="programming">Programming</option>
-                  <option value="javascript">JavaScript</option>
-                  <option value="rust">Rust</option>
-                  <option value="typescrit">TypeScript</option>
-                  <option value="mongo-db">Mongo DB</option>
-                  <option value="nodejs">NodeJs</option>
-                  <option value="react">React</option>
-                  <option value="nextjs">Nextjs</option>
-                </select>
                 <Editor setEditorState={setEditorState} editorState={editorState} />
 
-                <StyledButton disabled={!(isValid && dirty)} type="submit" primary title={<Text content={isLoginIn ? 'loading...' : 'Save'} fontWeight="600" />} />
+                <StyledButton disabled={!(isValid && dirty) || isSubmitting} type="submit" primary title={<Text content={isSubmitting ? 'loading...' : 'Save'} fontWeight="600" />} />
 
               </Container>
             </StyledForm>
